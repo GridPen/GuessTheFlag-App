a5e07063147b168cef9a631464a21c3fdd9c8756
Debashish M <10178321+GridPen@users.noreply.github.com>
2022-02-14 20:44:11 +0600

    few changes

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
     
     var body: some View {
         ZStack{
-            LinearGradient(gradient: Gradient(colors: [.mint, .blue]), startPoint: .top, endPoint: .bottom)
+            LinearGradient(gradient: Gradient(colors: [.indigo, .blue]), startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             VStack(spacing: 40){
                 VStack{
@@ -33,7 +33,7 @@ struct ContentView: View {
                     }label: {
                         Image(countries[number])
                             .renderingMode(.original)
-                            .cornerRadius(5)
+                            .cornerRadius(4)
                             .shadow(radius: 5)
                     }
                 }
