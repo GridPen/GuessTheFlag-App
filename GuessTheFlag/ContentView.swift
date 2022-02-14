4491d6f540cc5b810392b047267d82b06c2fdb46
Debashish M <10178321+GridPen@users.noreply.github.com>
2022-02-14 20:18:58 +0600

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,80 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Debashish Mondal on 1/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var playerScore = 0
+    
+    
+    var body: some View {
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [.mint, .blue]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            VStack(spacing: 40){
+                VStack{
+                    Text("Tap the flag off")
+                    Text(countries[correctAnswer])
+                        .font(.largeTitle.weight(.semibold))
+                        .foregroundStyle(.secondary)
+                        
+                }
+                ForEach(0..<3) {number in
+                    Button{
+                        ansTapped(number)
+                    }label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                            .cornerRadius(5)
+                            .shadow(radius: 5)
+                    }
+                }
+
+            }
+
+        }
+        
+        .alert(scoreTitle, isPresented: $showingScore){
+            Button("Next Quiz", action: refresh)
+        }message: {
+            Text("Your Score is \(playerScore)")
+        }
+    }
+    
+    
+
+    
+    func ansTapped(_ number : Int){
+        if correctAnswer == number{
+            showingScore = true
+            scoreTitle = "Correct Answer!"
+            playerScore += 1
+        }else{
+            scoreTitle = "Wrong Answer"
+            playerScore -= 1
+            showingScore = true
+
+        }
+    }
+    func refresh(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
+}
+
---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Debashish Mondal on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
